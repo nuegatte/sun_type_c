244b60dd883080de7dd034b0879a740d5831f808
nuegatte <105763071+nuegatte@users.noreply.github.com>
2025-10-03 18:24:21 +0800

    Added source files

---
+++ nuegatte/sun_type_c/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x25, 0x58, 0xE9, 0x3B, 0x7E, 0x85, 0xAA, 0xD3}
+#define NKRO_DEFAULT_ON
+#define VIAL_DYNAMIC_MACRO_COUNT 32
+#define DYNAMIC_MACRO_SIZE 512
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
